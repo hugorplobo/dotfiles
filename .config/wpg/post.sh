7342189d42f726cc5589b7ff74b6b7c66d2e4a1b
Hugo Lobo <hugorplobo@gmail.com>
2024-10-31 11:36:01 -0300

    update home path in wpg post script

--- .config/wpg/post.sh
+++ .config/wpg/post.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 killall waybar && waybar &
-swww img /home/hugo/.config/wpg/.current --transition-type wipe --transition-duration 1
-pywal-discord -t default -p /home/hugo/.config/vesktop/themes &
+swww img $HOME/.config/wpg/.current --transition-type wipe --transition-duration 1
+pywal-discord -t default -p $HOME/.config/vesktop/themes &
 walogram -s &
-/home/hugo/.config/mako/update-theme.sh &
+$HOME/.config/mako/update-theme.sh &
 pywalfox update
